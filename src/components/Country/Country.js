b67a71d34a4e1fa124cdf3ff3ff25986405a4fb3
Rajib-debnath1 <rajibdebnath519@gmail.com>
2023-03-23 13:55:01 +0600

    first commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    // console.log(props.country)
+    const {name, population, area, flags} = props.country;
+    return (
+        <div className='country bg-warning'>
+            <h2>{name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+        </div>
+    );
+};
+
+export default Country;
